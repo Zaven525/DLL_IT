4f0247816ee03225748294b57c357be32f2ba2e3
Zaven525 <71457724+Zaven525@users.noreply.github.com>
2026-02-28 13:17:52 +0400

    Add files via upload

--- list.hpp
+++ list.hpp
@@ -41,7 +41,7 @@ class List
         {
           private:
             Node<T>* curr;
-            friend class List;
+            friend class const_iterator;
           public:
             iterator(Node<T>* _curr) : curr{_curr} {}
             
@@ -55,9 +55,78 @@ class List
             T* operator->() const { return &curr->data; }
         };
 
+        class const_iterator
+        {
+          private:
+            const Node<T>* curr;
+            friend class List;
+          public:
+            const_iterator(const Node<T>* _curr) : curr{_curr} {}
+            const_iterator(const iterator& it) : curr(it.curr) {}
+
+            bool operator==(const const_iterator& other) const { return curr == other.curr; }
+            bool operator!=(const const_iterator& other) const { return curr != other.curr; }
+            const_iterator& operator++() { curr = curr->next; return *this; }
+            const_iterator operator++(int) { const_iterator tmp = *this; curr = curr->next; return tmp; }
+            const_iterator& operator--() { curr = curr->prev; return *this; }
+            const_iterator operator--(int) { const_iterator tmp = *this; curr = curr->prev; return tmp; }
+            const T& operator*() const { return curr->data; }
+            const T* operator->() const { return &curr->data; }
+        };
+
+        class reverse_iterator
+        {
+          private:
+            iterator it;
+            friend class const_reverse_iterator;
+          public:
+            reverse_iterator(const iterator& _it) : it{_it} {}
+            
+            bool operator==(const reverse_iterator& other) const { return it == other.it; }
+            bool operator!=(const reverse_iterator& other) const { return it != other.it; }
+            reverse_iterator& operator++() { --it; return *this; }
+            reverse_iterator operator++(int) { reverse_iterator tmp = *this; --it; return tmp; }
+            reverse_iterator& operator--() { ++it; return *this; }
+            reverse_iterator operator--(int) { reverse_iterator tmp = *this; ++it; return tmp; }
+            T& operator*() const 
+            { 
+              iterator tmp = it;
+              --tmp;
+              return *tmp; 
+            }
+            T* operator->() const { return &operator*(); }
+        };
+
+        class const_reverse_iterator
+        {
+          private:
+            const_iterator it;
+          public:
+            const_reverse_iterator(const const_iterator& _it) : it{_it} {}
+          
+            bool operator==(const const_reverse_iterator& other) const { return it == other.it; }
+            bool operator!=(const const_reverse_iterator& other) const { return it != other.it; }
+            const_reverse_iterator& operator++() { --it; return *this; }
+            const_reverse_iterator operator++(int) { const_reverse_iterator tmp = *this; --it; return tmp; }
+            const_reverse_iterator& operator--() { ++it; return *this; }
+            const_reverse_iterator operator--(int) { const_reverse_iterator tmp = *this; ++it; return tmp; }
+            const T& operator*() const 
+            { 
+              const_iterator tmp = it;
+              --tmp;
+              return *tmp; 
+            }
+            const T* operator->() const { return &operator*(); }
+        };
+
         iterator begin() const { return iterator(sentinel->next); }
         iterator end() const { return iterator(sentinel); }
-
+        const_iterator cbegin() const { return const_iterator(sentinel->next); }
+        const_iterator cend() const { return const_iterator(sentinel); }
+        reverse_iterator rbegin() const { return reverse_iterator(end()); }
+        reverse_iterator rend() const { return reverse_iterator(begin()); }
+        const_reverse_iterator crbegin() const { return const_reverse_iterator(cend()); }
+        const_reverse_iterator crend() const { return const_reverse_iterator(cbegin()); }
         //// Member functions
         List();
         List(size_t count, const T& _data = T());
